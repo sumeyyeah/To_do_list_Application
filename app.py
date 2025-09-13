3706a393e856ea4452a1d6754ef433939c07b7b2
Sumayya Shahul <sumayyashahul@Sumayyas-MacBook-Air.local>
2025-09-13 13:39:21 +0530

    Upload full folder

---
+++ app.py
@@ -0,0 +1,92 @@
+#Importing Packages
+
+from flask import Flask, render_template,redirect,request
+from flask_scss import Scss #Installed sass in VSCode 
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+#App
+
+app = Flask(__name__)
+Scss(app, static_dir='static', asset_dir='static')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todolist.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+#Data Class = Row of Data
+
+class MyTask(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(100), nullable=False)
+    create = db.Column(db.Integer,default=0)
+    created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return f"Task {self.id}"
+    
+
+with app.app_context():
+    db.create_all()
+
+
+
+@app.route("/",methods=["GET","POST"])
+def index():
+    
+    #Add a Task
+    if request.method == "POST":
+        current_task = request.form['content']
+        new_task = MyTask(content=current_task)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect("/")
+        except Exception as e:
+            print(f"Error: {e}")
+            return f"Error: {e}"
+    
+    #See All Current Tasks
+    else:
+        tasks = MyTask.query.order_by(MyTask.created).all()
+        return render_template("index.html",tasks=tasks)
+    
+
+#Delete a Item
+@app.route("/delete/<int:id>")
+def delete(id:int):
+    delete_task = MyTask.query.get_or_404(id)
+    try:
+        db.session.delete(delete_task)
+        db.session.commit()
+        return redirect("/")
+    except Exception as e:
+        return f"Error: {e}"
+
+
+
+
+#Edit an Item
+@app.route("/edit/<int:id>",methods=["GET","POST"])
+def edit(id:int):
+    task = MyTask.query.get_or_404(id)
+
+    if request.method == "POST":
+        task.content = request.form['content']
+        try:
+            db.session.commit()
+            return redirect("/")
+        except Exception as e:
+            return f"Error: {e}"
+    else:
+        return render_template("edit.html",task=task)
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
